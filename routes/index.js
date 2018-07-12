ff7e136f36517c713d950748b80b534e88edb3a6
Alite <Erion.Alite@jenzabar.com>
2018-07-12 22:28:06 +0100

    adding myhellonode to azure repo

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var model = {
+     title: 'Azure',
+     message:process.env.MESSAGE||"This is development"
+ };
+  res.render('index', model);
+});
+
+module.exports = router;
